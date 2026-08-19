b89b9b62f34981f88bf6cdf33229a2b548ea5d8f
saymoonislam009 <saymoonislam009@gmail.com>
2026-08-20 01:15:42 +0600

    Delete bariwala-app 3 directory

--- bariwala-app 3/lib/id.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
-const gen = customAlphabet(alphabet, 12);
-
-export function id(prefix: string) {
-  return `${prefix}_${gen()}`;
-}
